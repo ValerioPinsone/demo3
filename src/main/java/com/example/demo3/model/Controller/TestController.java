0fd9c8dd61bbf7f7b440fbc830e56289d60a753e
Valerio <valerio.pinsone@gmail.com>
2021-07-07 14:47:26 +0200

    test

--- src/main/java/com/example/demo3/model/Controller/TestController.java
+++ src/main/java/com/example/demo3/model/Controller/TestController.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class TestController {
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s!!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/test")
